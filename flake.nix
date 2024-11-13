f5f2c21d3ec9c27772fafe14de5485ed20018549
Ivan Mincik <ivan.mincik@gmail.com>
2024-11-13 20:07:52 +0100

    qgis: add plugins

--- flake.nix
+++ flake.nix
@@ -40,83 +40,118 @@
         };
 
         # Equivalent to  inputs'.nixpkgs.legacyPackages.hello;
-        packages = rec {
+        packages =
+          let
+            # qgis plugins
+            qgis-plugins =
+              let
+                plugins = import ./pkgs/qgis/qgis-plugins-list.nix;
+              in
+              pkgs.lib.mapAttrs'
+                (
+                  name: value: {
+                    name = "qgis-plugin-${name}";
+                    value = pkgs.callPackage ./pkgs/qgis/plugins.nix { name = name; plugin = value; };
+                  }
+                )
+                plugins;
 
-          # libs
-          gdal = pkgs.gdal;
-          gdal-minimal = pkgs.gdalMinimal;
-          gdal-master = (pkgs.callPackage ./pkgs/gdal/master.nix { inherit gdal; }).master;
+            qgis-ltr-plugins =
+              let
+                plugins = import ./pkgs/qgis/qgis-ltr-plugins-list.nix;
+              in
+              pkgs.lib.mapAttrs'
+                (
+                  name: value: {
+                    name = "qgis-ltr-plugin-${name}";
+                    value = pkgs.callPackage ./pkgs/qgis/plugins.nix { name = name; plugin = value; };
+                  }
+                )
+                plugins;
 
-          geos = pkgs.geos;
-          libgeotiff = pkgs.libgeotiff;
-          libLAS = pkgs.libLAS;
-          libosmium = pkgs.libosmium;
-          librttopo = pkgs.librttopo;
-          libspatialindex = pkgs.libspatialindex;
-          libspatialite = pkgs.libspatialite;
-          libtiff = pkgs.libtiff;
-          pdal = pkgs.pdal;
-          proj = pkgs.proj;
-          shapelib = pkgs.shapelib;
+          in
+          rec {
 
-          # Python packages
-          python3-fiona = pkgs.python3Packages.fiona;
-          python3-gdal = pkgs.python3Packages.gdal;
-          python3-geopandas = pkgs.python3Packages.geopandas;
-          python3-owslib = pkgs.python3Packages.owslib;
-          python3-psycopg = pkgs.python3Packages.psycopg;
-          python3-pyogrio = pkgs.python3Packages.pyogrio;
-          python3-pyproj = pkgs.python3Packages.pyproj;
-          python3-pystac = pkgs.python3Packages.pystac;
-          python3-rasterio = pkgs.python3Packages.rasterio;
-          python3-shapely = pkgs.python3Packages.shapely;
+            # libs
+            gdal = pkgs.gdal;
+            gdal-minimal = pkgs.gdalMinimal;
+            # FIXME: failing tests:
+            # * test_ogr_parquet_write_edge_cases
+            # * test_ogr_parquet_arrow_stream_empty_file
+            # gdal-master = (pkgs.callPackage ./pkgs/gdal/master.nix { }).master;
 
-          # PostgreSQL packages
-          postgresql-postgis = pkgs.postgresqlPackages.postgis;
+            geos = pkgs.geos;
+            libgeotiff = pkgs.libgeotiff;
+            libLAS = pkgs.libLAS;
+            libosmium = pkgs.libosmium;
+            librttopo = pkgs.librttopo;
+            libspatialindex = pkgs.libspatialindex;
+            libspatialite = pkgs.libspatialite;
+            libtiff = pkgs.libtiff;
+            pdal = pkgs.pdal;
+            proj = pkgs.proj;
+            shapelib = pkgs.shapelib;
 
-          # Tools
-          dcw-gmt = pkgs.dcw-gmt;
-          entwine = pkgs.entwine;
-          gmt = pkgs.gmt;
-          gshhg-gmt = pkgs.gshhg-gmt;
-          ili2c = pkgs.ili2c;
-          LAStools = pkgs.LAStools;
-          mapnik = pkgs.mapnik;
-          osm2pgsql = pkgs.osm2pgsql;
-          osmium-tool = pkgs.osmium-tool;
-          pmtiles = pkgs.pmtiles;
-          protozero = pkgs.protozero;
-          tippecanoe = pkgs.tippecanoe;
+            # Python packages
+            python3-fiona = pkgs.python3Packages.fiona;
+            python3-gdal = pkgs.python3Packages.gdal;
+            python3-geopandas = pkgs.python3Packages.geopandas;
+            python3-owslib = pkgs.python3Packages.owslib;
+            python3-psycopg = pkgs.python3Packages.psycopg;
+            python3-pyogrio = pkgs.python3Packages.pyogrio;
+            python3-pyproj = pkgs.python3Packages.pyproj;
+            python3-pystac = pkgs.python3Packages.pystac;
+            python3-rasterio = pkgs.python3Packages.rasterio;
+            python3-shapely = pkgs.python3Packages.shapely;
 
-          # Apps
-          grass = pkgs.grass;
-          openjump = pkgs.openjump;
-          qgis = pkgs.qgis;
-          qgis-ltr = pkgs.qgis-ltr;
-          saga = pkgs.saga;
-          spatialite-gui = pkgs.spatialite_gui; # FIXME: rename in nixpkgs
+            # PostgreSQL packages
+            postgresql-postgis = pkgs.postgresqlPackages.postgis;
 
-          # Services
-          geoserver = pkgs.geoserver;
-          mapcache = pkgs.mapcache;
-          mapproxy = pkgs.mapserver;
-          mapserver = pkgs.mapserver;
-          mbtileserver = pkgs.mbtileserver;
-          pg_featureserv = pkgs.pg_featureserv;
-          pg_tileserv = pkgs.pg_tileserv;
-          tegola = pkgs.tegola;
-          tile38 = pkgs.tile38;
-          # t-rex = pkgs.t-rex;
+            # Tools
+            dcw-gmt = pkgs.dcw-gmt;
+            entwine = pkgs.entwine;
+            gmt = pkgs.gmt;
+            gshhg-gmt = pkgs.gshhg-gmt;
+            ili2c = pkgs.ili2c;
+            LAStools = pkgs.LAStools;
+            mapnik = pkgs.mapnik;
+            osm2pgsql = pkgs.osm2pgsql;
+            osmium-tool = pkgs.osmium-tool;
+            pmtiles = pkgs.pmtiles;
+            protozero = pkgs.protozero;
+            tippecanoe = pkgs.tippecanoe;
 
-          # Other
-          nixGL = inputs'.nixgl.packages.nixGLIntel;
+            # Apps
+            grass = pkgs.grass;
+            openjump = pkgs.openjump;
+            qgis = pkgs.qgis;
+            qgis-ltr = pkgs.qgis-ltr;
+            saga = pkgs.saga;
+            spatialite-gui = pkgs.spatialite_gui; # FIXME: rename in nixpkgs
 
-          # Meta packages
-          all-packages = pkgs.symlinkJoin {
-            name = "all-packages";
-            paths = pkgs.lib.attrValues (pkgs.lib.filterAttrs (n: v: n != "all-packages") self'.packages);
-          };
-        };
+            # Services
+            geoserver = pkgs.geoserver;
+            mapcache = pkgs.mapcache;
+            mapproxy = pkgs.mapserver;
+            mapserver = pkgs.mapserver;
+            mbtileserver = pkgs.mbtileserver;
+            pg_featureserv = pkgs.pg_featureserv;
+            pg_tileserv = pkgs.pg_tileserv;
+            tegola = pkgs.tegola;
+            tile38 = pkgs.tile38;
+            # t-rex = pkgs.t-rex;
+
+            # Other
+            nixGL = inputs'.nixgl.packages.nixGLIntel;
+
+            # Meta packages
+            all-packages = pkgs.symlinkJoin {
+              name = "all-packages";
+              paths = pkgs.lib.attrValues (pkgs.lib.filterAttrs (n: v: n != "all-packages") self'.packages);
+            };
+          }
+          // qgis-plugins
+          // qgis-ltr-plugins;
 
         # Shells
         devShells = import ./shells.nix { inherit self' pkgs; };
