5790248aa1a33d2e970158a941a62e73bc52421c
Ivan Mincik <ivan.mincik@gmail.com>
2025-01-03 09:55:00 +0100

    libLAS: fix build with boost 1.86
    Nixpkgs PR:
    https://github.com/NixOS/nixpkgs/pull/369334

--- flake.nix
+++ flake.nix
@@ -230,6 +230,18 @@
 
               # nixGL
               nixGL = inputs.nixgl.packages.${prev.stdenv.hostPlatform.system}.nixGLIntel;
+
+              # Custom fixes
+              # libLAS
+              # https://github.com/NixOS/nixpkgs/pull/369334
+              libLAS = prev.libLAS.overrideAttrs (
+                old: {
+                  postPatch = ''
+                    substituteInPlace CMakeLists.txt \
+                      --replace-fail 'set(CMAKE_CXX_FLAGS "''${CMAKE_CXX_FLAGS} -std=c++98 -ansi")' '#'
+                  '';
+                }
+              );
             };
 
 
