9453d7124bda61d1964a61cc31ca51aacdb5ec4b
Ivan Mincik <ivan.mincik@gmail.com>
2025-05-22 13:00:08 +0200

    gdal-master: use full length commit hashes

--- pkgs/gdal/master-rev.nix
+++ pkgs/gdal/master-rev.nix
@@ -1,4 +1,4 @@
 {
-  rev = "ce2393a";
-  hash = "sha256-8E5OdYnejFsJ8bs7mygBKdGk7hAPYPZYxeQcGazPzpY=";
+  rev = "2f9b255b4c3e200435900c7605a8d42cc59f5f1d";
+  hash = "sha256-S5fdym3uq1h0bvMw01T3VZjy2HQlVWVpOQP1MCJA2J0=";
 }
