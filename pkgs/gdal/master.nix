90182c42ae8ae7c195958e4f00394e466a1c7e6f
Ivan Mincik <ivan.mincik@gmail.com>
2025-07-09 11:03:24 +0200

    gdal-master: disable failing test

--- pkgs/gdal/master.nix
+++ pkgs/gdal/master.nix
@@ -18,13 +18,16 @@ in
     patches = [ ];
 
     disabledTests = prev.disabledTests ++ [
-      # failing with master
+      # Failing in master
       # https://github.com/OSGeo/gdal/pull/10806#issuecomment-2362054085
       "test_ogr_gmlas_billion_laugh"
 
       # FIXME: started to fail since nixpkgs aebe249544837ce42588aa4b2e7972222ba12e8f
       "test_ogr_parquet_write_edge_cases"
       "test_ogr_parquet_arrow_stream_empty_file"
+
+      # Started to fail since 2025.27
+      "test_vrtrawlink_GDAL_VRT_RAWRASTERBAND_ALLOWED_SOURCE_ONLY_REMOTE_accepted"
     ];
   });
 }
