3842fcaa10fe42a00025c0dc698d15d19dbd15c4
imincik <imincik@users.noreply.github.com>
2025-08-28 01:35:35 +0000

    pkgs: weekly update of packages

--- pkgs/gdal/master-rev.nix
+++ pkgs/gdal/master-rev.nix
@@ -1,4 +1,4 @@
 {
-  rev = "f9bc129ef217428ee6266360216b31a2bc6c56d3";
-  hash = "sha256-0Ea+INPYuyks1U6qrZ20npJuxLjYEtcgVsO+AXAUO3Q=";
+  rev = "3343e7e1ea03aac67cfa2cb0ac763e6b2c84fbde";
+  hash = "sha256-LQ8qFC3uBg51FENdB7l7r/XNmd3HRBUbOTH6Z7aGOLw=";
 }
